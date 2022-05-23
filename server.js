8743381ad07d87aa949042bae9fae212ccc2d251
sarbajitmohanty <sarbajit.mohanty02@gmail.com>
2022-05-23 23:06:10 +0530

    models done

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 import express from "express";
 import mongoose from "mongoose";
-// import cors from "cors";
-// import dotenv from "dotenv";
-
-// import postRoutes from "./routes/posts.js";
-// import userRouter from "./routes/user.js";
 
 const app = express();
 dotenv.config();
@@ -12,9 +7,6 @@ dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
-// app.use("/posts", postRoutes);
-// app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello dorabhai API");
